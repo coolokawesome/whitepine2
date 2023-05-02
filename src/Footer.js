ddca48648a018979afbff02ecccbf6462297d7f2
rickyjoe11 <syrus.samson99@gmail.com>
2023-05-02 14:11:38 -0400

    added footer mark

--- src/Footer.js
+++ src/Footer.js
@@ -17,8 +17,9 @@ function Footer() {
                     <Link className='link-light mx-2 text-decoration-none' to='/faq'>FAQ</Link>
                     <Link className='link-light mx-2 text-decoration-none' to='/ourstory'>Our Story</Link>
                     </div>
+                    <p className=' text-center text-secondary my-1'>White Pine Tattoo © 2023</p>
                 </div>
-                <div className='col-12 col-lg-3 offset-0 offset-lg-1 py-3'>
+                <div className='col-12 col-lg-4 offset-0 offset-lg-1 py-3'>
                     <div className='row'>
                         <div className='col-12 text-light mb-3'>
                         <i class="fa-xl pt-3 pb-3 me-3 footer-icon fa-sharp fa-solid fa-location-dot"></i>309 King St., Midland, ON
@@ -28,12 +29,12 @@ function Footer() {
                         </div>
                     </div>
                 </div>  
-                <div className='col-12 col-lg-4 py-3'>
-                <p className='text-light'>Discover White Pine Tattoo, the 5-star rated tattoo shop in Midland, ON. We take pride in creating unforgettable, one-of-a-kind pieces for our clients. Book your consultation today and let us make your tattoo dreams a reality.</p>    
+                <div className='col-12 col-lg-3 py-3'>
+                <p className='text-light'>White Pine Tattoo, the 5-star rated tattoo shop in Midland, ON. Book your consultation today and let us make your tattoo dreams a reality.</p>    
                 </div>
             </div>
             <div className='my-2'>
-                <p className='text-light text-center my-1'>© 2023 White Pine Tattoo, Inc</p>
+                
                 <p className='text-light text-center'> All Rights Reserved</p>
             </div>
         </div>
